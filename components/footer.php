7fd99245c02a0df02424a3adeab8f90e97cd526a
ES <warrior1000w@gmail.com>
2024-04-14 15:02:36 +0600

    first commit

---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="+8801611441037"><i class="fas fa-phone"></i>+88 016 11 44 10 37 </a>
+         <a href="+8801984187765"><i class="fas fa-phone"></i>+88 019 84 18 77 65</a>
+         <a href="https://mail.google.com"><i class="fas fa-envelope"></i> tanvirahmedapu12@gmail.com</a>
+         <a href="https://www.google.com/maps/place/Gulshan,+Dhaka/@23.7897087,90.4104344,14z/data=!3m1!4b1!4m6!3m5!1s0x3755c7a0f70deb73:0x30c36498f90fe23!8m2!3d23.7924961!4d90.4078058!16s%2Fm%2F02phgdd?entry=ttu"><i class="fas fa-map-marker-alt"></i> Gulshan,Dhaka-1212</a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Tanvir Ahmed</span> | all rights reserved!</div>
+
+</footer>
